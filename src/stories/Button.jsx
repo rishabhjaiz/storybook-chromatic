26016ff95dd28590d1829d2e1ca9ef0c0c480865
Rishabh Jain <51243003+rishabhjaiz@users.noreply.github.com>
2022-01-21 22:18:05 +0530

    Remove a bug

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -18,11 +18,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-
-
-      {label}+'add'
-
-
+      {label}
     </button>
   );
 };
